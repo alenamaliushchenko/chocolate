8897af79933debce9d59147c0a5bec908439c30e
Alena Maliushchenko <alenamaliushchenko@gmail.com>
2024-12-10 10:29:49 +0200

    changes

--- form.js
+++ form.js
@@ -18,3 +18,36 @@ window.addEventListener('click', (event) => {
     modal.style.display = "none";
   }
 })
+
+
+
+// Форма підписки
+document.getElementById('emailForm').addEventListener('submit', function(event) {
+  event.preventDefault();
+
+  const emailInput = document.getElementById('email');
+  if (emailInput.checkValidity()) {
+    alert('Форма успішно відправлена!');
+    emailInput.value = '';
+  } else {
+    alert('Будь ласка, введіть коректну електронну пошту!');
+  }
+});
+
+
+// для динамічного переключення активної вкладки
+document.querySelectorAll('.header-link').forEach(element => {
+  element.addEventListener('click', function() {
+    document.querySelector('.header-link.active')?.classList.remove('active');
+    this.classList.add('active');
+  })
+});
+
+
+// для динамічного переключення активної вкладки в footer
+document.querySelectorAll('.footer-menu-link').forEach(element => {
+  element.addEventListener('click', function() {
+    document.querySelector('.footer-menu-link.active')?.classList.remove('active');
+    this.classList.add('active');
+  })
+});
