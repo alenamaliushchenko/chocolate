91c525e21916eca51c0446aaf416aa7d9500262a
Alena Maliushchenko <alenamaliushchenko@gmail.com>
2024-12-26 17:23:45 +0200

    changed styles of modal

--- form.js
+++ form.js
@@ -1,6 +1,7 @@
 const reviewBtn = document.getElementById("review-btn");
 const modal = document.getElementById("modal");
 const closeModalBtn = document.getElementById("modal-btn");
+const form = document.querySelector(".modal-form");
 
 // Функція для відкриття модального вікна
 reviewBtn.addEventListener('click', () => {
@@ -18,6 +19,54 @@ window.addEventListener('click', (event) => {
     modal.style.display = "none";
   }
 })
+form.addEventListener("submit", (event) => {
+  
+  const requiredFields = form.querySelectorAll("input[type='text'], input[type='email'], input[type='tel'], textarea");
+  const checkbox = form.querySelector(".accept-box");
+
+  let isValid = true;
+
+  requiredFields.forEach((field) => {
+    if (!field.value.trim()) {
+      isValid = false;
+      field.classList.add("input-error");
+    } else {
+      field.classList.remove("input-error");
+    }
+  });
+
+  if (!checkbox.checked) {
+    isValid = false;
+    checkbox.classList.add("input-error");
+  } else {
+    checkbox.classList.remove("input-error");
+  }
+
+  if (!isValid) {
+    event.preventDefault();
+    alert("Будь ласка, заповніть усі обов'язкові поля!");
+    return;
+  }
+
+  emailjs
+  .sendForm("service_kkdevin", "template_ltqawl9", form)
+  .then(() => {
+    alert("Ваше повідомлення надіслано!");
+    form.reset(); // Скидає значення форми
+    modal.style.display = "none"; // Закриває модальне вікно
+  })
+  .catch((error) => {
+    alert("Сталася помилка. Спробуйте ще раз.");
+    console.error("EmailJS Error:", error);
+  });
+});
+
+(function() {
+  emailjs.init("user_l5ZURSoXshmHs7-Fw"); // Замініть на ваш User ID з EmailJS
+})();
+
+
+
 
 
 
@@ -51,3 +100,25 @@ document.querySelectorAll('.footer-menu-link').forEach(element => {
     this.classList.add('active');
   })
 });
+
+
+
+// Завантаження YouTube API
+var tag = document.createElement('script');
+tag.src = "https://www.youtube.com/iframe_api";
+var firstScriptTag = document.getElementsByTagName('script')[0];
+firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+// Створення плеєра
+function onYouTubeIframeAPIReady() {
+  new YT.Player('youtube-player', {
+    videoId: 'LMPqAMfKakI', // ID вашого відео
+    playerVars: {
+      'autoplay': 0,
+      'controls': 1,
+      'modestbranding': 1,
+      'rel': 0, // Не показувати пов'язані відео
+      'showinfo': 0
+    }
+  });
+}
